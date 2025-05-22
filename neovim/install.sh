e1b26eff7daf32256a1cbaf7adf1c044780a90a8
Nathan Salberg <nate.salberg@gmail.com>
2025-05-21 20:17:53 -0500

    added neovim config and i3 mods

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,1 +1,5 @@
-sudo apt install neovim 
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt update
+sudo apt install make gcc ripgrep unzip git xclip neovim
+
+git clone https://github.com/NSalberg/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim 
